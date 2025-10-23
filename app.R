a75a7ee06b7e153afcc3cf71cd2e9cccd0750a4a
Camilotto Nicolas <ncamilotto@gmail.com>
2025-10-23 13:03:56 +0200

    fix object access

--- app.R
+++ app.R
@@ -1,18 +1,19 @@
 # Fichier : app.R
 # Point d'entrée pour Posit Connect
 
-# -- Étape 1 : Installation du package --
-# On s'assure que le package 'remotes' est disponible
+# -- Étape 1 : Installation du package (inchangée) --
+# On s'assure que 'remotes' est disponible
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
 }
-
-# On installe le package local (votre application) depuis le code source.
-# C'est l'équivalent de "Build > Install and Restart" dans RStudio,
-# mais exécuté sur le serveur de Posit Connect.
+# On installe le package local (votre application)
 remotes::install_local(upgrade = 'never')
 
-# -- Étape 2 : Lancement de l'application --
-# Maintenant que le package est bien installé, on le charge
-# et on exécute la fonction qui lance l'application Shiny.
-app.trustgame.appendix::run_app()
+# -- Étape 2 : Lancement de l'application (modifiée) --
+
+# On charge le package pour rendre ses fonctions ET SES DONNÉES disponibles
+library(app.trustgame.appendix)
+
+# On peut maintenant lancer l'application directement,
+# sans utiliser l'opérateur `::`
+run_app()
